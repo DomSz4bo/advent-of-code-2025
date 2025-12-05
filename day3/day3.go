2f2b37bca46b745f90762109afe8481b5725cb90
Dominik Szabó <szabo175@uniba.sk>
2025-12-05 21:54:18 +0100

    Add solution for day3 part 2

--- day3/day3.go
+++ day3/day3.go
@@ -6,14 +6,49 @@ import (
 	"os"
 )
 
-func maxJolt(battery string) int {
-	maxDigit := -1
-	maxDigitAfterMaxDigit := -1
-	length := len(battery)
+func fillArray[T any](arr []T, value T, fromIndex int) {
+	for i := fromIndex; i < len(arr); i++ {
+		arr[i] = value
+	}
+}
+
+func digitsToInt(digits []int) int {
+	result := 0
+	for _, d := range digits {
+		result = result*10 + d
+	}
+	return result
+}
+
+func maxJolt12(battery string) int {
+	numOfDigits := 12
+	maxDigits := make([]int, numOfDigits)
+	fillArray(maxDigits, -1, 0)
+	batteryLength := len(battery)
 
 	for i, x := range battery {
 		d := int(x - '0')
-		if maxDigit < d && i < length-1 {
+		for j := 0; j < numOfDigits; j++ {
+			if maxDigits[j] < d && i < batteryLength-11+j {
+				maxDigits[j] = d
+				fillArray(maxDigits, -1, j+1)
+				break
+			}
+		}
+	}
+
+	fmt.Println(maxDigits)
+	return digitsToInt(maxDigits)
+}
+
+func maxJolt(battery string) int {
+	maxDigit := -1
+	maxDigitAfterMaxDigit := -1
+	batteryLength := len(battery)
+
+	for i, x := range battery {
+		d := int(x - '0')
+		if maxDigit < d && i < batteryLength-1 {
 			maxDigit = d
 			maxDigitAfterMaxDigit = -1
 		} else if maxDigitAfterMaxDigit < d {
@@ -34,7 +69,7 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	joltSum := 0
 	for scanner.Scan() {
-		jolt := maxJolt(scanner.Text())
+		jolt := maxJolt12(scanner.Text())
 		joltSum += jolt
 	}
 	fmt.Println("Total output joltage is", joltSum)
